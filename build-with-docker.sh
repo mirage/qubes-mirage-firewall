b61527c9a9f598fed4f6095de48e4a6e8169e8bf
Thomas Leonard <thomas.leonard@unikernel.com>
2017-12-16 12:37:42 +0000

    Update to newly released version of netchannel

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -5,4 +5,4 @@ docker build -t qubes-mirage-firewall .
 echo Building Firewall...
 docker run --rm -i -v `pwd`:/home/opam/qubes-mirage-firewall qubes-mirage-firewall
 echo "SHA2 of build:   $(sha256sum qubes_firewall.xen)"
-echo "SHA2 last known: 2cad66c4b83817cdd1650f174586fd4daab7b7c271abd62844de6e6a17200750"
+echo "SHA2 last known: dc0e1e614e113b4e0d4fbd71e90d0489b3fc26a64cd1fbd0df8a56499dfa9a45"
