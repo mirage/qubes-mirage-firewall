ef09eb50ac883b6df000ff1a90138ecebc09a5a4
Ahmed Al-Sudani <git@ahmed.al-sudani.com>
2019-01-16 14:17:09 -0500

    Update last known build hash

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -5,4 +5,4 @@ docker build -t qubes-mirage-firewall .
 echo Building Firewall...
 docker run --rm -i -v `pwd`:/home/opam/qubes-mirage-firewall qubes-mirage-firewall
 echo "SHA2 of build:   $(sha256sum qubes_firewall.xen)"
-echo "SHA2 last known: dbc245bc425537082e64cf4b4822ce300ddeab10a272a009881e0bd22e06455a"
+echo "SHA2 last known: 3605a97fbdb9e699a9ceb9e43def8a3cdd04e5cefb48b5824df8f55e7f949203"
