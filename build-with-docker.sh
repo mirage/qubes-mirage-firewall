4526375a1915e34d763da5306f0793bd021fb312
Thomas Leonard <thomas.leonard@unikernel.com>
2019-01-19 10:32:27 +0000

    Note that Git versions might have different hashes

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -6,3 +6,4 @@ echo Building Firewall...
 docker run --rm -i -v `pwd`:/home/opam/qubes-mirage-firewall qubes-mirage-firewall
 echo "SHA2 of build:   $(sha256sum qubes_firewall.xen)"
 echo "SHA2 last known: 3605a97fbdb9e699a9ceb9e43def8a3cdd04e5cefb48b5824df8f55e7f949203"
+echo "(hashes should match for released versions)"
