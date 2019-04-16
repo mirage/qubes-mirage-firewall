45eef49c95048d5112257b8056c780f97ca58eb5
Thomas Leonard <talex5@gmail.com>
2019-04-16 18:05:08 +0100

    Upgrade to latest mirage-nat to fix ICMP
    Now ping and traceroute should work.

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -5,5 +5,5 @@ docker build -t qubes-mirage-firewall .
 echo Building Firewall...
 docker run --rm -i -v `pwd`:/home/opam/qubes-mirage-firewall qubes-mirage-firewall
 echo "SHA2 of build:   $(sha256sum qubes_firewall.xen)"
-echo "SHA2 last known: ce9a16b6f5ce0123f289b3586492f9f4b921f6e788f8e333784545807bb1b0f2"
+echo "SHA2 last known: 765cf16c2e85feb7e5dfd3e409a3013c91c2b07f5680ed9f4e487e27213f1355"
 echo "(hashes should match for released versions)"
