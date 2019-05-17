41126c9a6bb1f8e1b13ea551156b1bd408cc3b22
linse <sschirme@gmail.com>
2019-05-17 23:58:51 +0200

    Handle the exception to check if it was thrown when expected.

--- test/unikernel.ml
+++ test/unikernel.ml
@@ -2,18 +2,39 @@ open Lwt.Infix
 open Mirage_types_lwt
 open Printf
 
-let red fmt    = sprintf ("\027[31m"^^fmt^^"\027[m")
-let green fmt  = sprintf ("\027[32m"^^fmt^^"\027[m")
-let yellow fmt = sprintf ("\027[33m"^^fmt^^"\027[m")
-let blue fmt   = sprintf ("\027[36m"^^fmt^^"\027[m")
-
+(* TODO
+ * things we can have in rule
+ * - action: accept, drop
+ * - proto: None, TCP, UDP, ICMP
+ * - specialtarget: None, DNS
+ * - destination: Any, Some host
+ * - destination ports: possibly empty list of ranges
+ * - number (ordering over rules, to resolve conflicts by precedence)
+ *)
 (* Point-to-point links out of a netvm always have this IP TODO clarify with Marek *) 
 let uri = Uri.of_string "http://10.137.0.5:8082"
 
 module Client (T: TIME) (C: CONSOLE) (RES: Resolver_lwt.S) (CON: Conduit_mirage.S) = struct
 
-  let http_fetch c resolver ctx =
-    C.log c (yellow "Fetching %s with Cohttp:" (Uri.to_string uri)) >>= fun () ->
+  exception Check_error of string
+  let check_err fmt = Format.ksprintf (fun err -> raise (Check_error err)) fmt
+
+  let collect_exception f =
+    Lwt.try_bind f (fun _ -> Lwt.return None) (fun e -> Lwt.return (Some e))
+
+  let check_raises msg exn f =
+    collect_exception f >>= function
+    | None ->
+      check_err "Fail %s: expecting %s, got nothing." msg (Printexc.to_string exn)
+    | Some e when e <> exn ->
+      check_err "Fail %s: expecting %s, got %s."
+      msg (Printexc.to_string exn) (Printexc.to_string e)
+    | Some e ->
+      Format.printf "Exception as expected";
+      Lwt.return_unit
+
+  let http_fetch c resolver ctx () =
+    C.log c (sprintf "Fetching %s with Cohttp:" (Uri.to_string uri)) >>= fun () ->
     let ctx = Cohttp_mirage.Client.ctx resolver ctx in
     Cohttp_mirage.Client.get ~ctx uri >>= fun (response, body) ->
     Cohttp_lwt.Body.to_string body >>= fun body ->
@@ -25,6 +46,6 @@ module Client (T: TIME) (C: CONSOLE) (RES: Resolver_lwt.S) (CON: Conduit_mirage.
     C.log c (sprintf "Resolving using DNS server 8.8.8.8 (hardcoded)") >>= fun () ->
     (* wait a sec so we catch the output if it's fast *)
     OS.Time.sleep_ns (Duration.of_sec 1) >>= fun () ->
-    http_fetch c res ctx 
+    check_raises "fetch webpage, shold be denied" (Failure "TCP connection failed: connection attempt timed out") @@ http_fetch c res ctx 
 
 end
