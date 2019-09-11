c2bcf89057d8105bfc3c7bb3afa9097a47a86a92
linse <sschirme@gmail.com>
2019-09-11 19:42:31 +0200

    :x

--- resolver.ml
+++ resolver.ml
@@ -113,16 +113,29 @@ let ip_of_reply_packet (name : [`host] Domain_name.t) dns_packet =
   (* TODO: how should we handle authority? *)
   (* TODO: we need to handle other record types (CNAME, MX) ... *)
   | `Answer (answer, _authority) ->
-    begin
-      match Dns.Name_rr_map.find (Domain_name.raw name) Dns.Rr_map.A answer with
-      | Some q -> Ok q
-      | None ->
-        (* nethack.alt.org => CNAME someawshostnethack.alt.org
-         * in the mvar map, we have an entry with key nethack.alt.org and value some_mvar to wake up
-         * but we need to look up someawshostnethack.alt.org (and potentially more cnames beyond it) to get the ipv4 to compare with the packet
-         * so we need to have another structure that lets us map further cname responses (like someawshostnethack.alt.org) to the original request
-         * *)
-        Error `No_A_record
-    end
-  | _ -> Error  `Not_answer
-
+        let open Rresult in
+        let open Dns in
+    (* begin copied code from ocaml-dns client library for following cnames *)
+        let rec follow_cname counter q_name =
+          if counter <= 0 then Error (`Msg "CNAME recursion too deep")
+          else
+            Domain_name.Map.find_opt q_name answer
+            |> R.of_option ~none:(fun () ->
+                R.error_msgf "Can't find relevant map in response:@ \
+                              %a in [%a]"
+                  Domain_name.pp q_name
+                  (* TODO Mindy finds out if we need to deal with mx records *)
+                  Name_rr_map.pp answer
+              ) >>= fun relevant_map ->
+            begin match Rr_map.find A relevant_map with
+              | Some response -> Ok response
+              | None ->
+                begin match Rr_map.(find Cname relevant_map) with
+                  | None -> Error (`Msg "Invalid DNS response")
+                  | Some (_ttl, redirected_host) ->
+                    follow_cname (pred counter) redirected_host
+                end
+            end
+        in
+        follow_cname 20 (Domain_name.raw name)
+    (* end copied code *)
