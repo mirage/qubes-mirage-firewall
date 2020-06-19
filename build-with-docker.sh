931987190d62e2273e3241c46955a0b115d4eed8
linse <sschirme@gmail.com>
2020-06-19 09:48:16 +0000

    share host system opam cache with docker build
    Co-Authored-By: hannes <hannes@mehnert.org>

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -3,7 +3,7 @@ set -eu
 echo Building Docker image with dependencies..
 docker build -t qubes-mirage-firewall .
 echo Building Firewall...
-docker run --rm -i -v `pwd`:/home/opam/qubes-mirage-firewall qubes-mirage-firewall
+docker run --rm -i -v `pwd`:/home/opam/qubes-mirage-firewall -v ~/.opam/download-cache:/home/opam/.opam/download-cache qubes-mirage-firewall
 echo "SHA2 of build:   $(sha256sum qubes_firewall.xen)"
 echo "SHA2 last known: c2d7206d57e5977a608735af46e5ac5af0aa6cd5e052f0a177322dd76b67690c"
 echo "(hashes should match for released versions)"
