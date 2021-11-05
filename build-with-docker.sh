6835072104f2705ce56e0615255486c20c9ef13c
Hannes Mehnert <hannes@mehnert.org>
2021-11-05 19:39:10 +0100

    build-with-docker: update hash

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -5,5 +5,5 @@ docker build -t qubes-mirage-firewall .
 echo Building Firewall...
 docker run --rm -i -v `pwd`:/home/opam/qubes-mirage-firewall qubes-mirage-firewall
 echo "SHA2 of build:   $(sha256sum qubes_firewall.xen)"
-echo "SHA2 last known: d68d2a8d2337b8c1a78995e1acbb4f72082076c73be45bf40dd6268c87b2353e"
+echo "SHA2 last known: 2615ab9a9cbe5b29cf0d2a82aff7e281d06666da9cad5e767dbbc08acb77e295"
 echo "(hashes should match for released versions)"
