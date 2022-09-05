9b1b30aa2b45961da406de8a66b16db75b20ba98
palainp <pierre.alain@tuta.io>
2022-09-05 10:01:15 +0200

    trigger the GC earlier (at < 50% free space) print memory usage every 10 minutes

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -5,5 +5,5 @@ docker build -t qubes-mirage-firewall .
 echo Building Firewall...
 docker run --rm -i -v `pwd`:/home/opam/qubes-mirage-firewall qubes-mirage-firewall
 echo "SHA2 of build:   $(sha256sum ./dist/qubes-firewall.xen)"
-echo "SHA2 last known: 588e921b9d78a99f6f49d468a7b68284c50dabeba95698648ea52e99b381723b"
+echo "SHA2 last known: f77d17444edf299c64f12a62b6a9e2f598d166caf1bb7582dae4cab46f1dcb6d"
 echo "(hashes should match for released versions)"
