20ce084a496b06fb7f6290d38f8b54263a00589b
Hannes Mehnert <hannes@mehnert.org>
2022-11-11 13:37:43 +0100

    set netchannel + mirage-nat lower bounds

--- config.ml
+++ config.ml
@@ -24,11 +24,11 @@ let main =
       package ~min:"2.3.0" ~sublibs:["mirage"] "arp";
       package ~min:"3.0.0" "ethernet";
       package "shared-memory-ring" ~min:"3.0.0";
-      package "netchannel" ~min:"1.11.0";
+      package ~min:"2.1.2" "netchannel";
       package "mirage-net-xen";
       package "ipaddr" ~min:"5.2.0";
       package "mirage-qubes" ~min:"0.9.1";
-      package "mirage-nat" ~min:"3.0.0";
+      package ~min:"3.0.1" "mirage-nat";
       package "mirage-logs";
       package "mirage-xen" ~min:"8.0.0";
       package ~min:"6.4.0" "dns-client";
