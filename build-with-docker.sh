173832e053b3309f391772c3698eb1642b13e131
Pierre Alain <pierre.alain@tuta.io>
2023-10-13 09:21:40 +0200

    comply with SELinux enforcement AppVM

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -3,7 +3,7 @@ set -eu
 echo Building Docker image with dependencies..
 docker build -t qubes-mirage-firewall .
 echo Building Firewall...
-docker run --rm -i -v `pwd`:/tmp/orb-build qubes-mirage-firewall
+docker run --rm -i -v `pwd`:/tmp/orb-build:Z qubes-mirage-firewall
 echo "SHA2 of build:   $(sha256sum ./dist/qubes-firewall.xen)"
 echo "SHA2 last known: 8ae5314edf5b863b788c4b873e27bc4b206a2ff7ef1051c4c62ae41584ed3e14"
 echo "(hashes should match for released versions)"
