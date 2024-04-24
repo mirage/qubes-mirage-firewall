ba2a8731edc219d9eb39837ad66d5eef0ccb2245
Pierre Alain <pierre.alain@tuta.io>
2024-04-24 10:57:37 +0200

    update hashsum

--- build-with.sh
+++ build-with.sh
@@ -20,5 +20,5 @@ $builder build -t qubes-mirage-firewall .
 echo Building Firewall...
 $builder run --rm -i -v `pwd`:/tmp/orb-build:Z qubes-mirage-firewall
 echo "SHA2 of build:   $(sha256sum ./dist/qubes-firewall.xen)"
-echo "SHA2 last known: 2c3f68f49afdeaeedd2c03f8ef6d30d6bb4d6306bda0a1ff40f95f440a90034c"
+echo "SHA2 last known:  163991ea96842e03d378501a0be99057ad2489440aff8ae81d850624d98fd3f0"
 echo "(hashes should match for released versions)"
