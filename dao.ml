635f47e9151d86b525eb89ecf89b2f74445d45cf
Calascibetta Romain <romain.calascibetta@gmail.com>
2024-05-22 11:47:10 +0200

    Add a comment about our usage of List.hd (which can fail) and String.split_on_char

--- dao.ml
+++ dao.ml
@@ -81,6 +81,8 @@ let vifs client domid =
           let get_client_ip () =
             let* str = Xen_os.Xs.read handle (Fmt.str "%s/%d/ip" path device_id) in
             let client_ip = List.hd (String.split_on_char ' ' str) in
+            (* XXX(dinosaure): it's safe to use [List.hd] here,
+               [String.split_on_char] can not return an empty list. *)
             Lwt.return_some (vif, Ipaddr.V4.of_string_exn client_ip)
           in
           Lwt.catch get_client_ip @@ function
